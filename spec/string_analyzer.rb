a03101a3d005d841d06f89484b83272bca13ba65
Neil Rogers <rogerne@btinternet.com>
2019-04-23 13:01:20 +0100

    Files removed

--- spec/string_analyzer.rb
+++
@@ -1,9 +0,0 @@
-class StringAnalyzer
-    
-    def has_vowels?(str)
-      !!(str =~ /[aeiou]+/i)
-    end
-end
-
-sa = StringAnalyzer.new
-puts sa.has_vowels?('bc')
--- test.rb
+++
@@ -1,6 +0,0 @@
-class Test
-
-  def say_hello
-    "Hello"
-  end
-end
